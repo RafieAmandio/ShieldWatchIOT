a4cfded50c1538a2b1e2fe2a6be2953b186bd8ab
IdleGoat <rafie.amandio@ui.ac.id>
2023-12-08 21:09:36 +0700

    Add Photo

--- Source Code/Backend/src/controllers/Auth.controllers.js
+++ Source Code/Backend/src/controllers/Auth.controllers.js
@@ -1,20 +1,28 @@
-const authServices = require('../services/Auth.services');
+const authServices = require("../services/Auth.services");
 
 exports.register = async function (req, res) {
-    try {
-        const result = await authServices.register(req.body);
-        res.status(200).json(result);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
+  try {
+    const result = await authServices.register(req.body);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
+};
 
 exports.login = async function login(req, res) {
-    try {
-        const result = await authServices.login(req.body);
-        res.status(200).json(result);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-}
-  
+  try {
+    const result = await authServices.login(req.body);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+exports.addPhoto = async function addPhoto(req, res) {
+  try {
+    const result = await authServices.addPhoto(req);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
--- Source Code/Backend/src/routes/Auth.routes.js
+++ Source Code/Backend/src/routes/Auth.routes.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const fileMiddleware = require("../middleware/fileUpload.middleware");
+const authControllers = require("../controllers/Auth.controllers");
 
-const authControllers = require('../controllers/Auth.controllers');
-
-router.post('/register', authControllers.register);
-router.post('/login', authControllers.login);
+router.post("/register", authControllers.register);
+router.post("/login", authControllers.login);
+router.post("/addPhoto", fileMiddleware.uploadFiles, authControllers.addPhoto);
 
 module.exports = router;
