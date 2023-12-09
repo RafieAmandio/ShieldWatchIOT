55538f0d92824994344448c7273e5ae7bf9d632b
IdleGoat <rafie.amandio@ui.ac.id>
2023-12-09 13:01:00 +0700

    Login And Register Success

--- Source Code/Frontend/src/components/Login.js
+++ Source Code/Frontend/src/components/Login.js
@@ -1,79 +1,97 @@
 // src/components/Login.js
-import React, { useState } from 'react';
-import Navbar from '../components/Navbar';
+import React, { useState } from "react";
+import Navbar from "../components/Navbar";
+import axios from "axios";
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleLogin = async (event) => {
-        event.preventDefault();
-        try {
-            const response = await fetch("/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ email, password }),
-            });
-            const data = await response.json();
-            console.log(data);
-            if (response.ok) {
-                sessionStorage.setItem("isLogin", true);
-                window.alert("Logged in successfully");
-                // Perform any additional actions after successful login
-            } else {
-                window.alert("Email or password is incorrect");
-                // Handle the error scenario
-            }
-            window.location.href = "/";
-        } catch (error) {
-            console.error("Error logging in:", error);
-            // Handle the error scenario
-        }
-    };
+  const handleLogin = async (event) => {
+    event.preventDefault();
 
-    return (
-        <div>
-            <Navbar />
-            <div className="hero h-screen bg-white">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <form className="card-body text-white" onSubmit={handleLogin}>
-                            <h1 className="text-2xl font-bold ">Login</h1>
-                            <div className="form-control text-white">
-                                <label className="label">
-                                    <span className="label-text text-white">Email</span>
-                                </label>
-                                <input
-                                    type="text"
-                                    id="email"
-                                    className="mt-1 p-2 w-full border rounded"
-                                    value={email} onChange={(event) => setEmail(event.target.value)} required />
-                            </div>
-                            <div className="form-control text-white">
-                                <label className="label">
-                                    <span className="label-text text-white">Password</span>
-                                </label>
-                                <input
-                                    type="password"
-                                    id="password"
-                                    className="mt-1 p-2 w-full border rounded"
-                                    value={password} onChange={(event) => setPassword(event.target.value)} required />
-                                <label className="label mt-1 p-2">
-                                    <h1 className="text">Don't have an account? </h1>
-                                    <a href="/register" className="label link-hover">Sign Up</a>
-                                </label>
-                            </div>
-                            <div className="form-control">
-                                <button type="submit" className="btn bg-white text-black" onClick={handleLogin}>Sign in</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+    try {
+      const response = await axios.post("http://localhost:9696/auth/login", {
+        email,
+        password,
+      });
+      console.log(response);
+
+      const data = response.data.result;
+      console.log(data);
+
+      if (response.status === 200) {
+        sessionStorage.setItem("isLogin", true);
+        localStorage.setItem("user", JSON.stringify(data)); // Assuming the token is returned in the response
+        window.alert("Logged in successfully");
+        // Perform any additional actions after successful login
+      } else {
+        window.alert("Email or password is incorrect");
+        // Handle the error scenario
+      }
+
+      window.location.href = "/";
+    } catch (error) {
+      console.error("Error logging in:", error);
+      // Handle the error scenario
+    }
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <div className="hero h-screen bg-white">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body text-white" onSubmit={handleLogin}>
+              <h1 className="text-2xl font-bold ">Login</h1>
+              <div className="form-control text-white">
+                <label className="label">
+                  <span className="label-text text-white">Email</span>
+                </label>
+                <input
+                  type="text"
+                  id="email"
+                  className="mt-1 p-2 w-full border rounded"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                  required
+                />
+              </div>
+              <div className="form-control text-white">
+                <label className="label">
+                  <span className="label-text text-white">Password</span>
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  className="mt-1 p-2 w-full border rounded"
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                  required
+                />
+                <label className="label mt-1 p-2">
+                  <h1 className="text">Don't have an account? </h1>
+                  <a href="/register" className="label link-hover">
+                    Sign Up
+                  </a>
+                </label>
+              </div>
+              <div className="form-control">
+                <button
+                  type="submit"
+                  className="btn bg-white text-black"
+                  onClick={handleLogin}
+                >
+                  Sign in
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- Source Code/Frontend/src/pages/HomePage.js
+++ Source Code/Frontend/src/pages/HomePage.js
@@ -1,79 +1,96 @@
-import React, { useEffect, useState } from 'react';
-import mqtt from 'mqtt';
-import Navbar from '../components/Navbar';
-import AddDevice from '../components/AddDevice';
+import React, { useEffect, useState } from "react";
+import mqtt from "mqtt";
+import Navbar from "../components/Navbar";
+import AddDevice from "../components/AddDevice";
 
 const HomePage = () => {
-    const [pirValue, setPirValue] = useState(false);
-    const [temperatureValue, setTemperatureValue] = useState(0);
-    const [lightIntensityValue, setLightIntensityValue] = useState(0);
-    const [cameraValue, setCamera] = useState('');
+  const [pirValue, setPirValue] = useState(false);
+  const [temperatureValue, setTemperatureValue] = useState(0);
+  const [lightIntensityValue, setLightIntensityValue] = useState(0);
+  const [cameraValue, setCamera] = useState("");
 
-    useEffect(() => {
-        const client = mqtt.connect('mqtt://broker.mqttdashboard.com');
+  useEffect(() => {
+    const isLogin = sessionStorage.getItem("isLogin");
+    if (!isLogin) {
+      window.location.href = "/login";
+    }
 
-        client.on('connect', () => {
-            console.log('Connected to MQTT broker');
-            client.subscribe('ShieldWatchIOT', function (err) {
-                if (!err) {
-                    client.publish('ShieldWatchIOT', 'Hello from ShieldWatchIOT');
-                }
-            });
-        });
+    const client = mqtt.connect("mqtt://broker.mqttdashboard.com");
 
-        client.on('message', (topic, message) => {
-            // Assuming data format: { pir: boolean, temperature: number, lightIntensity: number, camera: string }
-            const data = JSON.parse(message.toString());
-            setPirValue(data.pir);
-            setTemperatureValue(data.temperature);
-            setLightIntensityValue(data.lightIntensity);
-            setCamera(data.camera);
-        });
+    client.on("connect", () => {
+      console.log("Connected to MQTT broker");
+      client.subscribe("ShieldWatchIOT", function (err) {
+        if (!err) {
+          client.publish("ShieldWatchIOT", "Hello from ShieldWatchIOT");
+        }
+      });
+    });
 
-        // Cleanup the MQTT client on component unmount
-        return () => {
-            client.end();
-        };
-    }, []);
+    client.on("message", (topic, message) => {
+      // Assuming data format: { pir: boolean, temperature: number, lightIntensity: number, camera: string }
+      const data = JSON.parse(message.toString());
+      setPirValue(data.pir);
+      setTemperatureValue(data.temperature);
+      setLightIntensityValue(data.lightIntensity);
+      setCamera(data.camera);
+    });
 
-    const stats = [
-        { id: 1, name: 'PIR Sensor', value: pirValue.toString() },
-        { id: 2, name: 'Temperature Sensor', value: `${temperatureValue} °C` },
-        { id: 3, name: 'Light Intensity Sensor', value: `${lightIntensityValue} lux` },
-    ];
+    // Cleanup the MQTT client on component unmount
+    return () => {
+      client.end();
+    };
+  }, []);
 
-    return (
-        <div>
-            <div className='h-screen'>
-                <Navbar />
-                <div className='bg-white flex items-end justify-end px-52'>
-                    <AddDevice />
-                </div>
-                <div className="h-full bg-white py-24 sm:py-8 h-">
-                    <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                        <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
-                            {stats.map((stat) => (
-                                <div key={stat.id} className="mx-auto flex max-w-xs flex-col gap-y-4">
-                                    <dt className="text-base leading-7 text-gray-600 font-medium">{stat.name}</dt>
-                                    <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
-                                        {stat.value}
-                                    </dd>
-                                </div>
-                            ))}
-                        </dl>
-                        <div className="card w-1/2 bg-white items-center mx-auto my-12">
-                            <div className="card-body">
-                                <h2 className="card-title text-5xl text-black">Camera</h2>
-                            </div>
-                            <figure>
-                                <img src=/*{cameraValue}*/ "https://media.suara.com/pictures/970x544/2022/11/17/50471-dinosaurus-di-kota-depok.jpg" alt="Camera" />
-                            </figure>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  const stats = [
+    { id: 1, name: "PIR Sensor", value: pirValue.toString() },
+    { id: 2, name: "Temperature Sensor", value: `${temperatureValue} °C` },
+    {
+      id: 3,
+      name: "Light Intensity Sensor",
+      value: `${lightIntensityValue} lux`,
+    },
+  ];
+
+  return (
+    <div>
+      <div className="h-screen">
+        <Navbar />
+        <div className="bg-white flex items-end justify-end px-52">
+          <AddDevice />
         </div>
-    );
+        <div className="h-full bg-white py-24 sm:py-8 h-">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+              {stats.map((stat) => (
+                <div
+                  key={stat.id}
+                  className="mx-auto flex max-w-xs flex-col gap-y-4"
+                >
+                  <dt className="text-base leading-7 text-gray-600 font-medium">
+                    {stat.name}
+                  </dt>
+                  <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                    {stat.value}
+                  </dd>
+                </div>
+              ))}
+            </dl>
+            <div className="card w-1/2 bg-white items-center mx-auto my-12">
+              <div className="card-body">
+                <h2 className="card-title text-5xl text-black">Camera</h2>
+              </div>
+              <figure>
+                <img
+                  src=/*{cameraValue}*/ "https://media.suara.com/pictures/970x544/2022/11/17/50471-dinosaurus-di-kota-depok.jpg"
+                  alt="Camera"
+                />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
