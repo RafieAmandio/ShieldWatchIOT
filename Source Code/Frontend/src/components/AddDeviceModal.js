1fac814ef2a207c5b4e2cd4681b1877832520585
fathanmuhandis <m.fathan11@ui.ac.id>
2023-12-10 23:37:17 +0700

    mencoba perbaiki add device

--- Source Code/Frontend/src/components/AddDeviceModal.js
+++ Source Code/Frontend/src/components/AddDeviceModal.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
-import { useForm } from 'react-hook-form';
+import axios from 'axios';
 
 const customStyles = {
     content: {
@@ -16,22 +16,27 @@ const customStyles = {
 };
 
 const AddDeviceModal = ({ isOpen, onRequestClose }) => {
-    const { register, handleSubmit } = useForm();
-    const [selectedDeviceType, setSelectedDeviceType] = useState('');
+    const [deviceName, setDeviceName] = useState("");
+    const [deviceID, setDeviceID] = useState("");
+    const [selectedDeviceType, setSelectedDeviceType] = useState("");
 
-    const addDevice = async (data) => {
-        const deviceData = { ...data, deviceType: selectedDeviceType };
+    const addDevice = async (event) => {
+        event.preventDefault();
 
         try {
-            const response = await fetch('/add', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(deviceData),
+            const response = await axios.post("http://localhost:9696/device/add", {
+                userId: JSON.parse(localStorage.getItem('user'))?.id,
+                device_name: deviceName,
+                device_id: deviceID,
+                type: selectedDeviceType,
             });
 
-            if (response.ok) {
+            console.log(response);
+
+            const data = response.data.result;
+            console.log(data);
+
+            if (response.status === 200) {
                 console.log('Device added successfully');
                 onRequestClose();
             } else {
@@ -48,55 +53,61 @@ const AddDeviceModal = ({ isOpen, onRequestClose }) => {
 
     return (
         <Modal isOpen={isOpen} onRequestClose={onRequestClose} style={customStyles}>
-            <div className="bg-white p-6 flex-row rounded-lg">
-                <h2 className="text-xl text-black text-center font-bold mb-4">Add Device</h2>
-                <form addDevice={handleSubmit(addDevice)}>
-                    <div className="mb-4">
-                        <label htmlFor="deviceName" className="block text-gray-700 font-semibold">Device Name:</label>
-                        <input
-                            type="text"
-                            id="deviceName"
-                            {...register('deviceName')}
-                            className="form-input mt-1 bg-white border-2 border-gray-400 block w-full text-black"
-                        />
-                    </div>
-
-                    <div className="mb-4">
-                        <label htmlFor="deviceID" className="block text-gray-700 font-semibold">Device ID:</label>
-                        <input
-                            type="text"
-                            id="deviceID"
-                            {...register('deviceID')}
-                            className="form-input mt-1 bg-white border-2 border-gray-400 block w-full text-black"
-                        />
-                    </div>
-
-                    <div className="mb-4">
-                        <label htmlFor="deviceType" className="block text-gray-700 font-semibold">Device Type:</label>
-                        <div className="dropdown dropdown-right">
-                            <div tabIndex={0} role="button" className="btn m-1">
-                                {selectedDeviceType ? selectedDeviceType : 'Choose'}
-                            </div>
-                            <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 ">
-                                <li
-                                    onClick={() => handleDeviceTypeSelect('Monitoring')}
-                                    className="hover:bg-gray-500 hover:text-white cursor-pointer label"
-                                >
-                                    Monitoring
-                                </li>
-                                <li
-                                    onClick={() => handleDeviceTypeSelect('Camera')}
-                                    className="hover:bg-gray-500 hover:text-white cursor-pointer label"
-                                >
-                                    Camera
-                                </li>
-                            </ul>
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="card shrink-0 w-full max-w-sm">
+                    <form className="card-body" onSubmit={addDevice}>
+                        <h2 className="text-xl font-bold text-center mb-4 text-black">Add Device</h2>
+                        <div className="mb-4 ">
+                            <label htmlFor="deviceName" className="block text-black font-semibold">Device Name:</label>
+                            <input
+                                type="text"
+                                id="deviceName"
+                                className="mt-1 p-2 w-full border rounded"
+                                value={deviceName}
+                                onChange={(event) => setDeviceName(event.target.value)}
+                            />
                         </div>
-                    </div>
-                    <button type="submit" className="btn text-white w-full mt-6">
-                        Add Device
-                    </button>
-                </form>
+
+                        <div className="mb-4">
+                            <label htmlFor="deviceID" className="block text-black font-semibold">Device ID:</label>
+                            <input
+                                type="text"
+                                id="deviceID"
+                                className="mt-1 p-2 w-full border rounded"
+                                value={deviceID}
+                                onChange={(event) => setDeviceID(event.target.value)}
+                            />
+                        </div>
+
+                        <div className="mb-4">
+                            <label htmlFor="deviceType" className="block text-black font-semibold">Device Type:</label>
+                            <div className="dropdown dropdown-right">
+                                <div tabIndex={0} role="button" className="btn m-1 bg-black">
+                                    {selectedDeviceType ? selectedDeviceType : 'Choose'}
+                                </div>
+                                <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-black rounded-box w-52 ">
+                                    <li
+                                        onClick={() => handleDeviceTypeSelect('Monitoring')}
+                                        className="hover:bg-gray-500 hover:text-white cursor-pointer label"
+                                    >
+                                        Monitoring
+                                    </li>
+                                    <li
+                                        onClick={() => handleDeviceTypeSelect('Camera')}
+                                        className="hover:bg-gray-500 hover:text-white cursor-pointer label"
+                                    >
+                                        Camera
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="form-control">
+                            <button type="submit" className="btn text-white w-full mt-6" onClick={addDevice}>
+                                Add Device
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </Modal>
     );
